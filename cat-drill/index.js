380752d5f0b6821dac6a3fd64403af2b804ab3b7
Alex Gutierrez <0alexgutierrez@gmail.com>
2018-08-23 12:06:15 -0700

    finished cat drill

---
+++ cat-drill/index.js
@@ -0,0 +1,16 @@
+function imageClick() {
+  $('.thumbnail').on('click', function(event) {
+    const imgSrc = $(this)
+      .find('img')
+      .attr('src');
+    const imgAlt = $(this)
+      .find('img')
+      .attr('alt');
+
+    $('.hero img')
+      .attr('src', imgSrc)
+      .attr('alt', imgAlt);
+  });
+}
+
+$(imageClick);
